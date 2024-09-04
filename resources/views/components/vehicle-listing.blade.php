2f1925a016b08ea96464bdf7762617e900f6f7ba
Kyle <148586193+thatdadbro@users.noreply.github.com>
2024-09-04 11:52:49 -0700

    Improve listing styles
    Vehicle listing component style updates. Upload images for my vehicles.

--- resources/views/components/vehicle-listing.blade.php
+++ resources/views/components/vehicle-listing.blade.php
@@ -13,11 +13,11 @@
     @php
         $imagePath = $vehicle->image ?? 'images/vehicles/default-vehicle-image.jpg';
     @endphp
-    <div>
-        <img src="{{ asset($imagePath) }}" alt="Image of {{ $vehicle->name }}" width="250px" style="padding-right:2rem;">
+    <div style="width:300px;">
+        <img src="{{ asset($imagePath) }}" alt="Image of {{ $vehicle->name }}" style="padding-right:2rem; width:100%">
     </div>
-    <div class="flex items-top">
-      <div style="margin-right:1.5rem; padding-right:1.5rem; border-right:1px solid #ddd;">
+    <div class="flex items-top w-full">
+      <div style="margin-right:1.5rem; padding-right:1.5rem; border-right:1px solid #ddd; width:40%">
           <h2 class="text-xl font-bold">{{ $vehicle->year }} {{ $vehicle->make }} {{ $vehicle->model }}</h2>
           <p class="text-md mb-2"><em>"{{ $vehicle->name }}"</em></p>
           <p class="text-gray-600 text-sm">Owner: {{ $vehicle->user->name }}</p>
@@ -27,9 +27,9 @@
           <p class="text-gray-600 text-sm">VIN: {{ $vehicle->vin ?? 'N/A' }}</p>
           <p class="text-gray-600 text-sm">License Plate: {{ $vehicle->license_plate ?? 'N/A' }}</p>
           <p class="text-gray-600 text-sm">Mileage: {{ $vehicle->mileage ?? 'N/A' }}</p>
-          <a href="#" class="text-blue-500 hover:underline text-sm">Read More</a>
+          <!-- <a href="#" class="text-blue-500 hover:underline text-sm">Read More</a> -->
       </div>
-      <div>
+      <div style="width:60%">
         <h3 class="mb-2">Description/Notes</h3>
         <p class="text-gray-600 text-sm">{{ $vehicle->description ?? 'N/A' }}</p>
       </div>
