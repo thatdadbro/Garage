31b0202cc18d800913b3d701100140d4595e16ff
Kyle <148586193+thatdadbro@users.noreply.github.com>
2024-09-05 14:59:20 -0700

    Vehicle mods models
    Adding the migration and models for vehicle modifications.

---
+++ resources/views/edit-vehicle.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Edit an Existing Vehicle
+        </h2>
+    </x-slot>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="py-12">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <x-forms.vehicle-form :action="route('vehicles.update')" :vehicle="{{ $vehicle }}" />
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
